ccaf7287b03e1209fd97c4d95f4d11a4247e550b
irene macapinlac <irenekaymac@gmail.com>
2020-03-05 01:11:17 +1100

    Updates

---
+++ precache-manifest.5ee3b72ff5cbc24257050128efc5bd08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8053b91b0dacfe190178269bf129e89",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c4264eb1e4a7d7a9faa2",
+    "url": "/portfolio/static/css/main.74fdb1f7.chunk.css"
+  },
+  {
+    "revision": "0aab1e5bfe140c097747",
+    "url": "/portfolio/static/js/2.77c1a0f1.chunk.js"
+  },
+  {
+    "revision": "c4264eb1e4a7d7a9faa2",
+    "url": "/portfolio/static/js/main.03b46895.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
