c43f44051fbd07a5e96c94c3990c2f6072104250
irene macapinlac <irenekaymac@gmail.com>
2020-03-11 23:14:57 +1100

    hover effect broke too much code

--- src/components/project.js
+++ src/components/project.js
@@ -7,27 +7,13 @@ class Project extends Component {
         return (
         <Fade bottom>
         <div className='project'>
-          <a href={this.props.url}>
-          </a>
-              <div className='box'>
-                <div className='imgBox'>
-                  <img src={this.props.imageSrc} alt={this.props.title}></img>
-                </div>
-                <div className='details'>
-                  <div className='content'>
-                    <h2>Tech Stack</h2>
-                    <p>{this.props.techStack}</p>
-                    <h3>Live Site</h3>
-                    <p>{this.props.url}</p>
-                    <h3>Code</h3>
-                    <p>{this.props.code}</p>
-
-                  </div>
-                </div>
-              </div>
+        <a href={this.props.url}>
+            <img src={this.props.imageSrc} alt={this.props.title}></img>
+            </a>
+            <h1>{this.props.title}</h1>
+            <span>{this.props.service}</span>
+            <span>{this.props.techStack}</span>
         </div>
-          <h1>{this.props.title}</h1>
-          <span>{this.props.service}</span>
             </Fade>);
     }
 }
